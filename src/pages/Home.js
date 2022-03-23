0f450f9750d6073ba1204ad6172cc39d348ef982
chandeepadissanayake <chandeepadissanayake@gmail.com>
2022-03-23 20:08:27 +0530

    Minor changes to home page cover.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,44 +1,42 @@
 import React from 'react';
 import {
-    MDBContainer
-  } from 'mdb-react-ui-kit';
+  MDBContainer
+} from 'mdb-react-ui-kit';
 
 import "../assets/css/pages/home.css";
 
 class Home extends React.Component {
 
-    render() {
-        return (
-            <header>
-      <div
-        id='homebg'
-        className='cover p-5 text-center bg-image'
-        /*style={{backgroundImage:"url(${bgcover})" }}*/
-      >
-        <div className='mask' style={{ backgroundColor: 'rgba(0, 0, 0, 0.7)' }}>
-          <div className='d-flex justify-content-center align-items-center h-100'>
-            <div className='text-white'>
-              <h1 className='title mb-3'>INVOTO</h1>
+  render() {
+    return (
+      <header>
+        <div
+          id='homebg'
+          className='cover p-5 text-center bg-image'
+        >
+          <div className='mask' style={{ backgroundColor: 'rgba(0, 0, 0, 0.7)' }}>
+            <div className='d-flex justify-content-center align-items-center h-100'>
+              <div className='text-white'>
+                <h1 className='title mb-3'>INVOTO</h1>
                 <div className='home-description'>
                   <h5 className='mb-3'>WHERE AI MEETS BILLS</h5>
-                    <p>Point your mobile device at the bill and can easily recognize
-                    different parts of the bill and interpret the contents.</p>
+                  <p>Invoto provides you an agent that interpretes your bills, for you.</p>
                 </div>
-              <a
-                className='btn btn-outline-light btn-lg m-2'
-                href='about-us'
-                role='button'
-                rel='nofollow'
-              >
-                ABOUT US
-              </a>
+                <a
+                  className='btn btn-outline-light btn-lg m-2'
+                  href='about-us'
+                  role='button'
+                  rel='nofollow'
+                >
+                  ABOUT US
+                </a>
+              </div>
             </div>
           </div>
-          </div>
-      </div>
-    </header>
-        );
-    }
+        </div>
+      </header>
+    );
+  }
 
 }
 
