89121769c8347a1b553d7601af87c5d25a460bd8
chandeepadissanayake <chandeepadissanayake@gmail.com>
2022-04-02 22:44:38 +0530

    Auth Checks added.

--- src/layouts/InvotoDashboardLayout.js
+++ src/layouts/InvotoDashboardLayout.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { useTheme, createTheme, ThemeProvider } from '@mui/material/styles';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import CssBaseline from '@mui/material/CssBaseline';
-import { Button, Tooltip } from '@mui/material';
+import { Tooltip } from '@mui/material';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import Toolbar from '@mui/material/Toolbar';
@@ -18,16 +18,20 @@ import useMediaQuery from '@mui/material/useMediaQuery';
 import { routesConfig, routesDashboard } from '../config/routes';
 import useToken from '../components/Dashboard/useToken';
 import { Navigate } from "react-router-dom";
+import { assessTokenValidity } from "../helpers/auth";
 
 const drawerWidth = 240;
 
 const ColorModeContext = React.createContext({ toggleColorMode: () => { } });
 
 function InvotoDashboard(props) {
+    const [body, setBody] = React.useState(null);
+    const { token, setToken } = useToken();
     const [mobileOpen, setMobileOpen] = React.useState(false);
     const [pageTitle, setPageTitle] = React.useState("");
     const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
     const [mode, setMode] = React.useState(prefersDarkMode ? 'dark' : 'light');
+
     const colorMode = React.useMemo(
         () => ({
             toggleColorMode: () => {
@@ -47,14 +51,6 @@ function InvotoDashboard(props) {
         [mode],
     );
 
-    const { token, setToken } = useToken();
-
-    if (!token) {
-        return (
-            <Navigate replace to="/accounts" />
-        );
-    }
-
     const handleDrawerToggle = () => {
         setMobileOpen(!mobileOpen);
     };
@@ -63,71 +59,95 @@ function InvotoDashboard(props) {
         setPageTitle: setPageTitle,
     };
 
+    const handleLogout = () => {
+        setToken(null);
+        setBody(
+            <Navigate replace to="/accounts" />
+        );
+    }
+
+    React.useEffect(() => {
+        assessTokenValidity(token).then((isTokenValid) => {
+            if (!isTokenValid) {
+                setBody(
+                    <Navigate replace to="/accounts" />
+                );
+            }
+        });
+    }, []);
+
     return (
-        <DashboardProvider value={contextElements}>
-            <ColorModeContext.Provider value={colorMode}>
-                <ThemeProvider theme={theme}>
-                    <Box sx={{ display: 'flex' }}>
-                        <CssBaseline />
-                        <AppBar
-                            position="fixed"
-                            sx={{
-                                width: { sm: `calc(100% - ${drawerWidth}px)` },
-                                ml: { sm: `${drawerWidth}px` },
-                            }}
-                        >
-                            <Toolbar>
-                                <IconButton
-                                    color="inherit"
-                                    aria-label="open drawer"
-                                    edge="start"
-                                    onClick={handleDrawerToggle}
-                                    sx={{ mr: 2, display: { sm: 'none' } }}
-                                >
-                                    <MenuIcon />
-                                </IconButton>
-                                <Typography variant="h6" noWrap component="div" sx={{ flexGrow: 1 }}>
-                                    {pageTitle}
-                                </Typography>
+        <div>
+            <DashboardProvider value={contextElements}>
+                <ColorModeContext.Provider value={colorMode}>
+                    <ThemeProvider theme={theme}>
+                        <Box sx={{ display: 'flex' }}>
+                            <CssBaseline />
+                            <AppBar
+                                position="fixed"
+                                sx={{
+                                    width: { sm: `calc(100% - ${drawerWidth}px)` },
+                                    ml: { sm: `${drawerWidth}px` },
+                                }}
+                            >
+                                <Toolbar>
+                                    <IconButton
+                                        color="inherit"
+                                        aria-label="open drawer"
+                                        edge="start"
+                                        onClick={handleDrawerToggle}
+                                        sx={{ mr: 2, display: { sm: 'none' } }}
+                                    >
+                                        <MenuIcon />
+                                    </IconButton>
+                                    <Typography variant="h6" noWrap component="div" sx={{ flexGrow: 1 }}>
+                                        {pageTitle}
+                                    </Typography>
 
-                                <Box>
-                                    <Tooltip title={theme.palette.mode + " mode"}>
-                                        <IconButton sx={{ ml: 1 }} onClick={colorMode.toggleColorMode} color="inherit">
-                                            {theme.palette.mode === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}
-                                        </IconButton>
-                                    </Tooltip>
-                                </Box>
+                                    <Box>
+                                        <Tooltip title={theme.palette.mode + " mode"}>
+                                            <IconButton sx={{ ml: 1 }} onClick={colorMode.toggleColorMode} color="inherit">
+                                                {theme.palette.mode === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}
+                                            </IconButton>
+                                        </Tooltip>
+                                    </Box>
 
-                                <Box>
-                                    <Tooltip title="Logout">
-                                        <IconButton sx={{ ml: 1 }} color="inherit">
-                                            <LogoutIcon />
-                                        </IconButton>
-                                    </Tooltip>
-                                </Box>
-                            </Toolbar>
-                        </AppBar>
+                                    <Box>
+                                        <Tooltip title="Logout">
+                                            <IconButton sx={{ ml: 1 }} onClick={handleLogout} color="inherit">
+                                                <LogoutIcon />
+                                            </IconButton>
+                                        </Tooltip>
+                                    </Box>
+                                </Toolbar>
+                            </AppBar>
 
-                        <Box
-                            component="nav"
-                            sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
-                            aria-label="mailbox folders"
-                        >
-                            <AdminSidebar handleDrawerToggle={handleDrawerToggle} drawerWidth={drawerWidth} mobileOpen={mobileOpen} rootRoute={routesConfig.roots.dashboard} routes={routesDashboard} />
+                            <Box
+                                component="nav"
+                                sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+                                aria-label="mailbox folders"
+                            >
+                                <AdminSidebar handleDrawerToggle={handleDrawerToggle} drawerWidth={drawerWidth} mobileOpen={mobileOpen} rootRoute={routesConfig.roots.dashboard} routes={routesDashboard} />
+                            </Box>
+
+                            <Box
+                                component="main"
+                                sx={{ flexGrow: 1, p: 3, width: { sm: `calc(100% - ${drawerWidth}px)` } }}
+                            >
+                                <Toolbar />
+                                <Outlet />
+                            </Box>
                         </Box>
+                    </ThemeProvider>
+                </ColorModeContext.Provider>
+            </DashboardProvider>
 
-                        <Box
-                            component="main"
-                            sx={{ flexGrow: 1, p: 3, width: { sm: `calc(100% - ${drawerWidth}px)` } }}
-                        >
-                            <Toolbar />
-                            <Outlet />
-                        </Box>
-                    </Box>
-                </ThemeProvider>
-            </ColorModeContext.Provider>
-        </DashboardProvider >
+            <div>
+                {body}
+            </div>
+        </div>
     );
+    return body;
 }
 
 export default InvotoDashboard;
